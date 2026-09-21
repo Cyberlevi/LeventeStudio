475e129f2dd40883a04d50656b3662ace3683cc4
Levi <93142137+Cyberlevi@users.noreply.github.com>
2026-09-21 18:53:49 +0200

    fix: declare portrait intrinsic size on about page

--- src/components/AboutExpertDark.tsx
+++ src/components/AboutExpertDark.tsx
@@ -25,6 +25,8 @@ export default function AboutExpertDark() {
               <img
                 data-portrait-image
                 src="/levente_studio_portrait_final.webp"
+                width={800}
+                height={533}
                 className="h-full w-full object-cover grayscale transition-[filter,transform] duration-700 ease-out group-hover:scale-[1.008] group-hover:grayscale-[.9]"
                 alt="Tarnóczi Levente – weboldalkészítő"
                 decoding="async"
